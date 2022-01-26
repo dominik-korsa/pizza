85cf84be50a6a724de37d961e98de69c37309821
Dominik Korsa <dominik.korsa@gmail.com>
2022-01-26 13:49:51 +0100

    Add support for multiple Discord servers

--- server/discordCommands.ts
+++ server/discordCommands.ts
@@ -3,66 +3,71 @@ import {requireEnv} from "./utils";
 
 export async function registerDiscordCommands() {
     const slashCommandClient = new InteractionsClient(requireEnv('DISCORD_TOKEN'), requireEnv('DISCORD_CLIENT_ID'));
-    const guildId = requireEnv('DISCORD_GUILD_ID');
+    await Promise.all(
+        requireEnv('DISCORD_GUILD_ID')
+            .split(',')
+            .map((id) => id.trim())
+            .map(async (guildId) => {
+                await slashCommandClient.createCommand({
+                    name: 'link',
+                    description: 'Powiąż swoje konto na Discordzie z imieniem i nazwiskiem',
+                    options: [
+                        {
+                            name: 'name',
+                            description: 'Imię i nazwisko',
+                            type: 3,
+                            required: true,
+                        },
+                        {
+                            name: 'class',
+                            description: 'Klasa',
+                            type: 3,
+                            required: true,
+                        },
+                    ]
+                }, guildId);
 
-    await slashCommandClient.createCommand({
-        name: 'link',
-        description: 'Powiąż swoje konto na Discordzie z imieniem i nazwiskiem',
-        options: [
-            {
-                name: 'name',
-                description: 'Imię i nazwisko',
-                type: 3,
-                required: true,
-            },
-            {
-                name: 'class',
-                description: 'Klasa',
-                type: 3,
-                required: true,
-            },
-        ]
-    }, guildId);
+                await slashCommandClient.createCommand({
+                    name: 'order',
+                    description: 'Dodaj swoje imię i nazwisko do listy zamówień',
+                    options: [
+                        {
+                            name: 'drink',
+                            description: 'Czy chcesz zamówić napój?',
+                            choices: [
+                                {
+                                    name: 'Własny kubek',
+                                    value: 'Własny kubek',
+                                },
+                                {
+                                    name: 'Jednorazowy kubek',
+                                    value: 'Jednorazowy kubek',
+                                },
+                                {
+                                    name: 'Bez napoju',
+                                    value: 'Bez napoju',
+                                }
+                            ],
+                            type: 3,
+                            required: true,
+                        },
+                    ]
+                }, guildId);
 
-    await slashCommandClient.createCommand({
-        name: 'order',
-        description: 'Dodaj swoje imię i nazwisko do listy zamówień',
-        options: [
-            {
-                name: 'drink',
-                description: 'Czy chcesz zamówić napój?',
-                choices: [
-                    {
-                        name: 'Własny kubek',
-                        value: 'Własny kubek',
-                    },
-                    {
-                        name: 'Jednorazowy kubek',
-                        value: 'Jednorazowy kubek',
-                    },
-                    {
-                        name: 'Bez napoju',
-                        value: 'Bez napoju',
-                    }
-                ],
-                type: 3,
-                required: true,
-            },
-        ]
-    }, guildId);
-
-    await slashCommandClient.createCommand({
-        name: 'complete',
-        description: 'Zakończ zamówienie przez dodanie liczby zjedzonych kawałków',
-        options: [
-            {
-                name: 'pieces',
-                description: 'Liczba kawałków',
-                type: 4,
-                // @ts-ignore
-                min_value: 0, // see https://github.com/MatteZ02/discord-interactions/issues/23
-                required: true,
-            },
-        ]
-    }, guildId);
+                await slashCommandClient.createCommand({
+                    name: 'complete',
+                    description: 'Zakończ zamówienie przez dodanie liczby zjedzonych kawałków',
+                    options: [
+                        {
+                            name: 'pieces',
+                            description: 'Liczba kawałków',
+                            type: 4,
+                            // @ts-ignore
+                            min_value: 0, // see https://github.com/MatteZ02/discord-interactions/issues/23
+                            required: true,
+                        },
+                    ]
+                }, guildId);
+            })
+    );
 }
